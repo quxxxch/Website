adf947aa3dfbe6083cf3a82aa0e57a746604e74d
manhdoi <manhtran40kc@gmail.com>
2024-05-25 16:16:06 +0700

    cicd

---
+++ README.md
@@ -0,0 +1,11 @@
+## Docker build
+
+```shell
+docker build -f Dockerfile -t website:latest .
+```
+
+## Docker run
+
+```shell
+docker run -d -it --rm -p 3000:3000 --name my_website website:latest
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,30 @@
-const path = require('path');
+const path = require("path");
+const webpack = require('webpack');
+
+const JS_DIR = path.resolve(__dirname, './public_html');
+const BUILD_DIR = path.resolve(__dirname, './dist');
 
 module.exports = {
-  mode: 'development', // Set to 'development' or 'production' as needed
-  entry: './index.js', // Ensure this path is correct
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist') // Output directory
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
- 
-      }
-    ]
-  }
+    entry: {
+        feedback: [`${JS_DIR}/feedback.js`],
+    },
+    output: {
+        path: BUILD_DIR,
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+
+    plugins: []
 };
